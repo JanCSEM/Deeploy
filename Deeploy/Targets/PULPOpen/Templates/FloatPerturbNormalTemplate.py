85219d413cbe3982107a1b012dc6f58bb6ca35aa
JanCSEM <jan.snoeijs@csem.ch>
2026-02-28 07:04:35 +0000

    Port Noise kernels to GAP9

--- Deeploy/Targets/PULPOpen/Templates/FloatPerturbNormalTemplate.py
+++ Deeploy/Targets/PULPOpen/Templates/FloatPerturbNormalTemplate.py
@@ -7,7 +7,7 @@ from typing import Dict, List, Tuple
 from Deeploy.DeeployTypes import NetworkContext, NodeTemplate, OperatorRepresentation
 
 
-class _FloatPerturbEggrollTemplate(NodeTemplate):
+class _FloatPerturbNormalTemplate(NodeTemplate):
 
     def __init__(self, templateStr):
         super().__init__(templateStr)
@@ -20,8 +20,8 @@ class _FloatPerturbEggrollTemplate(NodeTemplate):
 
 
 # TODO: No loop unrolling optimization yet
-referenceTemplate = _FloatPerturbEggrollTemplate("""
-// Perturb Eggroll (Name: ${nodeName}, Op: ${nodeOp})
+referenceTemplate = _FloatPerturbNormalTemplate("""
+// PerturbNormal (Name: ${nodeName}, Op: ${nodeOp})
 uint8_t ${nodeName}_core_id = (uint8_t) pi_core_id();
 uint8_t ${nodeName}_log2Core = (uint8_t) log2(NUM_CORES);
 uint32_t ${nodeName}_chunk = (${size} >> ${nodeName}_log2Core) + ((${size} & (NUM_CORES-1))!=0);
@@ -29,9 +29,16 @@ uint32_t ${nodeName}_chunk_start = (uint32_t) MIN(${nodeName}_chunk*${nodeName}_
 uint32_t ${nodeName}_chunk_stop = (uint32_t) MIN(${nodeName}_chunk_start + ${nodeName}_chunk, (uint32_t) ${size});
 uint32_t ${nodeName}_local_size = ${nodeName}_chunk_stop - ${nodeName}_chunk_start;
 
-uint32_t chunk_seed = seed +  (${nodeName}_chunk_start * ${node_id}) + (${node_id} * 104729);
+uint32_t chunk_seed = seed + (${nodeName}_chunk_start * ${node_id}) + (${node_id} * 104729);
 
-GenEggrollPerturbation((float32_t *) &${data_out}[${nodeName}_chunk_start],
-                        chunk_seed,
-                        ${nodeName}_local_size);
-""")
+// pick large enough stride to minimize correlation between nodes.
+ApplyGaussianPerturbation(
+    (const float32_t *) &${data_in}[${nodeName}_chunk_start],
+    (float32_t *) &${data_out}[${nodeName}_chunk_start],
+    chunk_seed,
+    perturbation_sign, // globally defined in DeedeployTest main
+    ${nodeName}_local_size,
+    ${eps}f,
+);
+"""
+)
--- Deeploy/Targets/PULPOpen/Templates/FloatPerturbTriangleTemplate.py
+++ Deeploy/Targets/PULPOpen/Templates/FloatPerturbTriangleTemplate.py
@@ -7,7 +7,7 @@ from typing import Dict, List, Tuple
 from Deeploy.DeeployTypes import NetworkContext, NodeTemplate, OperatorRepresentation
 
 
-class _FloatPerturbEggrollTemplate(NodeTemplate):
+class _FloatPerturbTriangleTemplate(NodeTemplate):
 
     def __init__(self, templateStr):
         super().__init__(templateStr)
@@ -20,8 +20,8 @@ class _FloatPerturbEggrollTemplate(NodeTemplate):
 
 
 # TODO: No loop unrolling optimization yet
-referenceTemplate = _FloatPerturbEggrollTemplate("""
-// Perturb Eggroll (Name: ${nodeName}, Op: ${nodeOp})
+referenceTemplate = _FloatPerturbTriangleTemplate("""
+// PerturbTriangle (Name: ${nodeName}, Op: ${nodeOp})
 uint8_t ${nodeName}_core_id = (uint8_t) pi_core_id();
 uint8_t ${nodeName}_log2Core = (uint8_t) log2(NUM_CORES);
 uint32_t ${nodeName}_chunk = (${size} >> ${nodeName}_log2Core) + ((${size} & (NUM_CORES-1))!=0);
@@ -29,9 +29,27 @@ uint32_t ${nodeName}_chunk_start = (uint32_t) MIN(${nodeName}_chunk*${nodeName}_
 uint32_t ${nodeName}_chunk_stop = (uint32_t) MIN(${nodeName}_chunk_start + ${nodeName}_chunk, (uint32_t) ${size});
 uint32_t ${nodeName}_local_size = ${nodeName}_chunk_stop - ${nodeName}_chunk_start;
 
-uint32_t chunk_seed = seed +  (${nodeName}_chunk_start * ${node_id}) + (${node_id} * 104729);
+uint32_t i = ${nodeName}_chunk_start;
+for (; i < ${nodeName}_chunk_stop; i++) {
+    // pick large enough stride to minimize correlation between nodes.
+    uint32_t chunk_seed = seed + i*${nodeName}_chunk_start + (${node_id} * 104729);
+    ApplyTrianglePerturbation((const float32_t *)  &${data_in}[i],
+                                (float32_t *) &${data_out}[i],
+                                chunk_seed,
+                                perturbation_sign, // globally defined in DeedeployTest main
+                                ${nodeName}_local_size,
+                                ${eps}f);
+}
+""")
 
-GenEggrollPerturbation((float32_t *) &${data_out}[${nodeName}_chunk_start],
-                        chunk_seed,
-                        ${nodeName}_local_size);
+updateTemplate = _FloatPerturbTriangleTemplate("""
+// UpdateTriangle (Name: ${nodeName}, Op: ${nodeOp})
+BEGIN_SINGLE_CORE
+    UpdateWeightsTriangle((float32_t *)${data_in},
+                                loss,
+                                seed + ${node_id},
+                                ${eps}f,
+                                lr, // globally defined
+                                ${size});
+END_SINGLE_CORE
 """)
